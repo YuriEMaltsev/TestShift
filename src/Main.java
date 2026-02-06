4c46ed3386521431abeee64214ce93dd42037698
Yuriy Maltsev <ye.maltsev@yandex.ru>
2026-02-06 12:21:10 +0700

    c0206 - 2 Cash

--- src/Main.java
+++ src/Main.java
@@ -8,58 +8,39 @@ public class  Main {
 
     // разбор строки
     static short ParseStr(String str) {
-       final short max_double = 4; // максимальная точность double
-       final short a_minus = 45;  // "-"
-       final short a_point = 46;  // "."
-       final short a_0 = 48;      // "0"
-       final short a_9 = 57;      // "9"
 
-       short ret = 3;        // код завершения 1-long, 2-double, 3- String
-       short f_lead_minus=0; // встречен лидирующий минус
-       short f_digit = 0;    // 1 -встречена цифра
-       short f_point = 0;    // 1 -встречена точка
-       short f_count_p =0;   // 1 -17 - количество десятичных знаков показателя степени точки
-       short f_e = 0;        // 1 -встречена E или e (показатель степени)
-       short f_e_minus=0;    // 1 -встречен  минус после E
-       short f_count_e=0;    // 1,2 - количество десятичных знаков показателя степени после E-
+       final short r_int    = 1;   // Int
+       final short r_double = 2;   // Double
+       final short r_string = 3;   // String
 
-       int a=0; // код ASCII
-       int i=0;
-       for ( ; i< str.length(); i++) {
-           a = (int) str.charAt(i);
-           if (i == 0 || a == a_minus) { // встречен лидирующий минус
-               f_lead_minus = 1;
-               continue;
-           }
-           if (a >= a_0 && a <= a_9) { // встречен символы 0..9
-               f_digit = 1;
-               if (f_point == 1) f_count_p++;
-               if (f_count_p > max_double) break; // максимальная точность double
-               continue;
-           }
-           else if (a == a_point){
-               if(f_point == 1) break; // если встречена вторая точка - выход
-               f_point = 1;
-               continue;
-           }
-           // если не обработано - это строка!
-           break ;
-       }
+       short ret = r_string;
 
-       // проверка флагов, если просмотрены все символы
-       if (i == str.length()) {
-           if ( f_point == 0 )
-             ret = 1;
-           else {
-             if (f_count_p >0)
-                 ret = 2;
-           }
-       }
-       return ret;
+        // пробую Long
+        try {
+            long l = Long.parseLong(str);
+            System.out.println("Int l = " + l);
+            return(r_int);
+        } catch (NumberFormatException nfe) {
+        }
+
+        // пробую Double
+        try {
+            Double d = Double.parseDouble(str);
+            System.out.println("double d = " + d);
+            return(r_double);
+        } catch (NumberFormatException nfe) {
+            //System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+
+        // если не long b не double, то String
+        return ret;
+
+
+
     }
 
     public static void main(String[] args) {
-          String test="-1008.96";
+          String test="-1008.0E2";
           short res;
           res = ParseStr(test);
           System.out.printf("\nres="+res);
